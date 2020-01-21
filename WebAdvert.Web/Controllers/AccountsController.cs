86c9a8e45085cb0a43e3cce389142841de76a782
Yankov.Vyacheslav <slava.brigida@nixsolutions.com>
2020-01-21 21:13:41 +0200

    Initial commit with initial cognito integration

---
+++ WebAdvert.Web/Controllers/AccountsController.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading.Tasks;
+using Amazon.AspNetCore.Identity.Cognito;
+using Amazon.Extensions.CognitoAuthentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebAdvert.Web.Models;
+
+namespace WebAdvert.Web.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private SignInManager<CognitoUser> _signInManager;
+        private readonly UserManager<CognitoUser> _userManager;
+        private readonly CognitoUserPool _pool;
+
+        public AccountsController(SignInManager<CognitoUser> signInManager,
+            UserManager<CognitoUser> userManager,
+            CognitoUserPool pool)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _pool = pool;
+        }
+        
+        public IActionResult SignUp()
+        {
+            return View(new SignupModel());
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignupModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _pool.GetUser(model.Email);
+                if (user.Status != null)
+                {
+                    ModelState.AddModelError("UserExists", "User with this email already exists");
+                    return View(model);
+                }
+
+                user.Attributes.Add(CognitoAttribute.Name.AttributeName, model.Email);
+                
+                var createdUser = await _userManager.CreateAsync(user, model.Password);
+
+                if (createdUser.Succeeded)
+                {
+                    return RedirectToAction("Confirm");
+                }
+            }
+
+            return View();
+        }
+    }
+}
